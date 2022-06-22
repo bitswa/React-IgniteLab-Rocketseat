9f5b9bc57dacc87f2e34b2f2da24355dc5a92ceb
bitswa <bitswa@pm.me>
2022-06-22 20:39:38 -0300

    3 day

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
-import { Event } from './pages/Event';
+import { ApolloProvider } from "@apollo/client";
+import { Event } from "./pages/Event";
+import { Router } from "./Router";
+import { client } from "./lib/apollo";
+import { BrowserRouter } from "react-router-dom";
 
-function App() {
+export function App() {
   return (
     <div className="App">
-      <Event />
+      <ApolloProvider client={client}>
+        <BrowserRouter>
+          <Router />
+        </BrowserRouter>
+      </ApolloProvider>
     </div>
-  )
+  );
 }
-
-export default App
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -3,30 +3,28 @@ import { Lesson } from "./Lesson";
 
 const GET_LESSONS_QUERY = gql`
   query {
-  lessons(orderBy: availableAt_ASC, stage: PUBLISHED) {
-    id
-    lessonType
-    availableAt
-    slug
-    title
+    lessons(orderBy: availableAt_ASC, stage: PUBLISHED) {
+      id
+      lessonType
+      availableAt
+      slug
+      title
+    }
   }
-}
-
-`
+`;
 
 interface GetLessonsQueryResponse {
   lessons: {
-    id: string
-    title: string
-    slug: string
-    availableAt: string 
-    lessonType: 'live' | 'class'
-  }[]
+    id: string;
+    title: string;
+    slug: string;
+    availableAt: string;
+    lessonType: "live" | "class";
+  }[];
 }
 
-
 export function Sidebar() {
-  const { data }= useQuery<GetLessonsQueryResponse>(GET_LESSONS_QUERY)
+  const { data } = useQuery<GetLessonsQueryResponse>(GET_LESSONS_QUERY);
 
   return (
     <aside className="w-[348px] bg-gray-700 p-6 border-l border-gray-600">
@@ -35,16 +33,16 @@ export function Sidebar() {
       </span>
 
       <div className="flex flex-col gap-8">
-        {data?.lessons.map(lesson => {
+        {data?.lessons.map((lesson) => {
           return (
-            <Lesson 
+            <Lesson
               key={lesson.id}
               title={lesson.title}
               slug={lesson.slug}
               availableAt={new Date(lesson.availableAt)}
               type={lesson.lessonType}
             />
-          )
+          );
         })}
       </div>
     </aside>
